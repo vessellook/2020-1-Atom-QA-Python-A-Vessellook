beeedc8ec94aec78fbd1b6cfd2f9cd24391c356d
vessellook <vessellook@gmail.com>
2020-10-11 13:06:06 +0300

    Allure instructions added

--- hw2/code/tests/test_basic.py
+++ hw2/code/tests/test_basic.py
@@ -1,8 +1,10 @@
 import pytest
+import allure
 from ui.pages.main_page import MainPage
-from ui.pages.create_campaign_page import CreateCampaignPage
 from ui.pages.dashboard_page import DashboardPage
+from ui.pages.create_campaign_page import CreateCampaignPage
 from ui.pages.segments_page import SegmentsPage
+from variables import EMAIL, PASSWORD
 
 from ui.utils import generate_campaign_name, generate_segment_name
 
@@ -15,7 +17,9 @@ def test_main_title(main_page: MainPage):
 
 # @pytest.mark.skip(reason="It's working fine")
 @pytest.mark.UI
-def test_auth(dashboard_page: DashboardPage):
+def test_auth(main_page: MainPage):
+    with allure.step('Auth to target.my.com'):
+        dashboard_page = main_page.auth(EMAIL, PASSWORD)
     assert dashboard_page.is_url_matches('target.my.com/dashboard')
 
 
@@ -23,13 +27,16 @@ def test_auth(dashboard_page: DashboardPage):
 @pytest.mark.UI
 @pytest.mark.parametrize('email, password', [('email-invalid', 'some-password')])
 def test_auth_negative(main_page: MainPage, email, password):
-    not_dashboard_page: DashboardPage = main_page.auth(email, password)
+    with allure.step(f'Auth with wrong credentials {email, password} to target.my.com'):
+        not_dashboard_page = main_page.auth(email, password)
     assert not not_dashboard_page.is_url_matches('target.my.com/dashboard')
 
 
 # @pytest.mark.skip(reason="It's working fine")
 @pytest.mark.UI
-def test_create_campaign(create_campaign_page: CreateCampaignPage):
+def test_create_campaign(dashboard_page: DashboardPage):
+    with allure.step(f'Open CreateCampaignPage'):
+        create_campaign_page = dashboard_page.create_campaign()
     assert create_campaign_page.is_url_matches()
 
 
@@ -49,10 +56,12 @@ def test_create_campaign(create_campaign_page: CreateCampaignPage):
 # @pytest.mark.parametrize('site_url', ['https://vk.com'])
 def test_create_site_traffic_campaign(create_campaign_page: CreateCampaignPage, site_url):
     campaign_name = generate_campaign_name()
-    dashboard_page: DashboardPage = create_campaign_page.traffic(site_url, campaign_name, 500, 10000)
+    with allure.step(f'create campaign with name {campaign_name}'):
+        dashboard_page = create_campaign_page.traffic(site_url, campaign_name, 500, 10000)
     dashboard_page.refresh()
     assert dashboard_page.has_campaign(campaign_name)
-    dashboard_page.remove_campaign(campaign_name)
+    with allure.step(f'remove campaign with name {campaign_name}'):
+        dashboard_page.remove_campaign(campaign_name)
     dashboard_page.refresh()
     assert not dashboard_page.has_campaign(campaign_name)
 
@@ -61,8 +70,10 @@ def test_create_site_traffic_campaign(create_campaign_page: CreateCampaignPage, 
 @pytest.mark.UI
 def test_create_segment(segments_page: SegmentsPage):
     segment_name = generate_segment_name()
-    segments_page.create_segment(segment_name)
+    with allure.step(f'create segment with name {segments_page}'):
+        segments_page.create_segment(segment_name)
     assert segments_page.has_segment(segment_name)
-    segments_page.remove_segment(segment_name)
+    with allure.step(f'remove segment with name {segments_page}'):
+        segments_page.remove_segment(segment_name)
     segments_page.refresh()
     assert not segments_page.has_segment(segment_name)
