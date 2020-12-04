fcd8f14244b2c7e9e3c510eccda5ba6ce50c6cdb
vessellook <vessellook@gmail.com>
2020-12-04 21:15:31 +0300

    Add initial query file for MySQL

---
+++ final/configs/init_query.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `test_users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(64) NOT NULL,
+  `access` smallint DEFAULT NULL,
+  `active` smallint DEFAULT NULL,
+  `start_active_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `ix_test_users_username` (`username`)
+);
